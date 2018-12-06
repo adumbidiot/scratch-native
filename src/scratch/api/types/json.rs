585f133cd9b27f0170e8448d74b793eb97db92b3
adumbidiot <nathaniel.daniel23@outlook.com>
2018-12-05 23:46:28 -0800

    Began work on custom Script deserializer

--- src/scratch/api/types/json.rs
+++ src/scratch/api/types/json.rs
@@ -1,3 +1,11 @@
+use serde::{Deserializer, Deserialize, Serialize, Serializer};
+use serde::de::{MapAccess, SeqAccess, Visitor};
+
+use serde_json::Value;
+
+use std::collections::HashMap;
+use std::marker::PhantomData;
+
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct ProjectJson {
 	#[serde(rename = "objName")]
@@ -45,10 +53,28 @@ pub struct SpriteJson{
 	#[serde(rename = "scratchY")]
 	pub y: f64,
 	
+	#[serde(rename = "currentCostumeIndex")]
+	pub current_costume_index: u64,
+	
+	#[serde(rename = "isDraggable")]
+	is_draggable: bool,
+	
+	#[serde(rename = "rotationStyle")]
+	rotation_style: String,
+	
+	pub visible: bool,
+	pub scale: f64,
+	pub direction: f64,
+	
 	pub costumes: Vec<CostumeJson>,
 	
 	#[serde(default)]
 	pub sounds: Vec<SoundJson>,
+	
+	scripts: Option<Vec<ScriptJson>>,
+	
+	#[serde(flatten)]
+    unknown: HashMap<String, Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -96,3 +122,75 @@ pub struct StatsJson{
 	remixes: u64,
 	views: u64,
 }
+
+#[derive(Debug, Default, Serialize)]
+pub struct ScriptJson{
+	x: f64,
+	y: f64,
+	blocks: Vec<Vec<String>>
+}
+
+impl<'d> Deserialize<'d> for ScriptJson {
+    fn deserialize<D>(deserializer: D) -> Result<ScriptJson, D::Error>
+        where D: Deserializer<'d>
+    {
+		deserializer.deserialize_any(BlockListOrStruct)
+    }
+}
+
+struct BlockListOrStruct;
+
+impl<'de> Visitor<'de> for BlockListOrStruct {
+        type Value = ScriptJson;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("Struct or Array")
+        }
+		
+		fn visit_seq<A>(self, mut seq: A) -> Result<ScriptJson, A::Error>
+		where
+			A: SeqAccess<'de>,
+        {
+			let x: f64 = seq.next_element()?.ok_or(serde::de::Error::invalid_length(0, &self))?;
+			let y: f64 = seq.next_element()?.ok_or(serde::de::Error::invalid_length(1, &self))?;
+			let blocks: Vec<Vec<String>> = seq.next_element()?.ok_or(serde::de::Error::invalid_length(2, &self))?;
+			return Ok(ScriptJson{
+				x,
+				y,
+				blocks,
+			});
+        }
+		
+		fn visit_map<M>(self, mut map: M) -> Result<ScriptJson, M::Error>
+            where M: serde::de::MapAccess<'de>
+        {
+			let mut x = None;
+			let mut y = None;
+			let mut blocks = None;
+			while let Some(key) = map.next_key()? {
+				match key {
+					"x" => {
+						x = Some(map.next_value()?);
+					},
+					"y" => {
+						y = Some(map.next_value()?);
+					},
+					"blocks" => {
+						blocks = Some(map.next_value()?);
+					},
+					_=>{
+					
+					}
+                }
+            }
+			
+			let x = x.unwrap();
+			let y = y.unwrap();
+			let blocks = blocks.unwrap();
+            return Ok(ScriptJson{
+				x,
+				y,
+				blocks,
+			});
+        }
+    }
